10fa08350ab8ab481a79fb719d8bdcac8655cbd5
Asadbek <asadbekraimov642@gmail.com>
2022-03-28 07:39:45 +0500

    asaf

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/Heroes'));
+app.use(express.static(__dirname + '/dist/heroes'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname+'/dist/Heroes/index.html'));
+res.sendFile(path.join(__dirname+'/dist/heroes/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
